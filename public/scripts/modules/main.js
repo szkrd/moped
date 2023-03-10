15361e06e094f4666d31b93a63ff8a1351283a71
Szabolcs Kurdi <szabolcs.kurdi@gmail.com>
2023-03-10 16:37:08 +0100

    better time formatting and external links

--- public/scripts/modules/main.js
+++ public/scripts/modules/main.js
@@ -1,5 +1,9 @@
 import { escapeHtml, getCurrentSongName } from './utils.js';
 
+const { dayjs } = window;
+dayjs.extend(window.dayjs_plugin_relativeTime);
+dayjs.extend(window.dayjs_plugin_duration);
+
 const SIO_DEBOUNCE = 1000;
 let loadCount = 0;
 
@@ -25,25 +29,35 @@ function getJSON(url, onSuccess) {
 }
 
 function updateJsonDataUi(prefix, obj) {
+  const deprecated = ['time'];
   const subTypeDate = ['dbUpdate'];
-  const subTypeTime = ['playtime', 'uptime'];
+  const subTypeTime = ['playtime', 'uptime', 'elapsed', 'dbPlaytime'];
+  const subTypeRelativeDate = ['at'];
   const html = [];
   Object.keys(obj).forEach((key) => {
     const val = obj[key];
     let className = [typeof val];
     let text = val;
+    let formattedText = '';
     let subType = '';
+    if (deprecated.includes(key)) return;
     if (subTypeDate.includes(key) && typeof val === 'number') {
       subType = 'date';
       text = window.dayjs(val * 1000).format('YYYY MMM. D. / HH:mm');
     }
-    if (subTypeTime.includes(key)) subType = 'time';
+    if (subTypeTime.includes(key)) {
+      subType = 'time';
+      formattedText = dayjs(val * 1000).format('HH:mm:ss'); // from seconds
+    }
+    if (subTypeRelativeDate.includes(key)) subType = 'relativeDate';
     if (subType) className.push(subType);
     if (val !== false) {
       html.push(`
       <li>
-      <label>${escapeHtml(key)}</label>
-      <span class="${className.join(' ')}">${escapeHtml(text)}</span>
+        <label>${escapeHtml(key)}</label>
+        <span class="${className.join(' ')}" data-key="${key}" data-value="${escapeHtml(text)}">
+          ${escapeHtml(formattedText || text)}
+        </span>
       </li>
       `);
     }
@@ -56,7 +70,11 @@ function refreshStats(subsystem = 'all') {
     getJSON('/api/status/current-song', (resp) => {
       const title = getCurrentSongName(resp);
       document.title = title || 'moped';
-      $('.current-song-name').text(title);
+      $('.current-song-name').text(title || '-');
+      $('.external-search')[title ? 'show' : 'hide'];
+      $('.search-provider').each((idx, el) => {
+        $(el).attr('href', el.dataset.href.replace('%SEARCH%', encodeURIComponent(title)));
+      });
       updateJsonDataUi('current-song', resp);
     });
   }
@@ -106,8 +124,19 @@ function onBodyClick(evt) {
   }
 }
 
+function refreshRealitveTimes() {
+  setInterval(() => {
+    $('span[data-key="at"]').each((idx, el) => {
+      el = $(el);
+      const val = el.data('value');
+      if (/^\d+$/.test(val)) el.text(dayjs(val).fromNow() + ` (${val})`);
+    });
+  }, 1000);
+}
+
 export default function main() {
   $('body').on('click', onBodyClick);
+  refreshRealitveTimes();
   setupSocketIo();
   refreshStats();
 }
